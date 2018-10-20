aede18de229a7bc478636096c46c80b783ce6271
Fernando Christyanto <christyantofernando@gmail.com>
2018-10-21 04:54:16 +0700

    Fix landing page to mockup

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import LandingpageHeader from './components/header/landingpageHeader'
+import Footer from './components/footer/Footer'
+import steveJobs from './assets/img/Steve-Jobs-PNG-Image-92168.png'
+import BlueButton from './components/buttons/BlueButton'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ maxWidth: '1300px', margin: 'auto' }}>
+        <LandingpageHeader />
+        <content style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ padding: '6rem' }}>
+            <h1 style={{ color: '#289ad6' }}>RuangBelajar.com</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum laborum in porro illo dolorum molestias ullam, quia omnis ducimus illum tenetur debitis ad ea asperiores odio modi quos nihil fugit?
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere aliquam iusto cupiditate minus molestiae, tenetur voluptate laborum! Saepe expedita corporis suscipit sint eveniet mollitia fuga dolorem ad earum nobis. Doloribus?
+            </p>
+            <BlueButton
+              buttonText="Read More" />
+          </div>
+          <div>
+            <img src={steveJobs} alt="" />
+          </div>
+        </content>
+        <Footer />
       </div>
     );
   }
